04bc3e65f310003423e69bc14aaf0b8af7dda752
Level Antoine <level.toine@gmail.com>
2021-05-28 11:34:30 +0200

    init Voiture + getters

---
+++ htdocs/tp2/Voiture.php
@@ -0,0 +1,32 @@
+<?php
+
+class Voiture{
+    private $_couleur;
+    private $_puissance;
+    private $_vitesse;
+
+    /**
+     * @return mixed
+     */
+    public function getCouleur()
+    {
+        return $this->_couleur;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPuissance()
+    {
+        return $this->_puissance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVitesse()
+    {
+        return $this->_vitesse;
+    }
+
+}
