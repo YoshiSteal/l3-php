077160dfe7e38ea8408d0e65cdaa142c0e38e896
Level Antoine <level.toine@gmail.com>
2021-06-18 11:01:12 +0200

    add ToDoController

---
+++ htdocs/tp3/App/Controller/ToDoController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Controller;
+
+use GuzzleHttp\Client;
+
+class ToDoController extends AbstractController
+{
+
+    public function home()
+    {
+        // Use guzzle call api todo
+        // https://jsonplaceholder.typicode.com/todos
+    }
+
+}
