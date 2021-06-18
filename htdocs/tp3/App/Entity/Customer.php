aa1878b0d375cdbb3d52a55b5341cc0116e721d4
Level Antoine <level.toine@gmail.com>
2021-06-18 10:27:45 +0200

    customers

--- htdocs/tp3/App/Entity/Customer.php
+++ htdocs/tp3/App/Entity/Customer.php
@@ -6,5 +6,57 @@ namespace App\Entity;
 
 class Customer implements EntityInterface
 {
+    private $nom;
+    private $prenom;
+    private $age;
 
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age): void
+    {
+        $this->age = $age;
+    }
+
+    
 }
