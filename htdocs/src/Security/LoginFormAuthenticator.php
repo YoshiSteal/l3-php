01153dea5fce934c78d6f0e1e72d86ebc28b42b7
Level Antoine <level.toine@gmail.com>
2021-07-08 12:01:11 +0200

    register

--- htdocs/src/Security/LoginFormAuthenticator.php
+++ htdocs/src/Security/LoginFormAuthenticator.php
@@ -100,7 +100,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
         // redirect to some "app_homepage" route - of wherever you want
-        return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl(): string
