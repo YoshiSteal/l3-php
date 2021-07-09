5824b927375e0e92a36583bae5d4a5767d71ce99
Level Antoine <level.toine@gmail.com>
2021-07-09 10:51:02 +0200

    profil done

--- htdocs/src/Controller/ProfilController.php
+++ htdocs/src/Controller/ProfilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Pari;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,12 @@ class ProfilController extends AbstractController
      */
     public function index(): Response
     {
+        $json = file_get_contents('../matchs.json');
+        $matchs = json_decode($json, true);
+        $paris = $this->getDoctrine()->getRepository(Pari::class)->findBy(['id_user' => $this->getUser()->getId()]);
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'paris' => $paris,
+            'matchs' => $matchs
         ]);
     }
 }
